91a27b57e6a3b1881d53064991db109a25d8d411
disanda <skyanda1@qq.com>
2020-09-20 21:47:25 +0800

    x

--- train.py
+++ train.py
@@ -189,9 +189,11 @@ if __name__ == '__main__':
 	                writer.add_scalar('G/%s' % k, v.data.cpu().numpy(), global_step=it_g)
 	        # sample
 	        #if it_g % 100 == 0:
-	        if True:
-	            x_fake = (sample(z)+1)/2
-	            torchvision.utils.save_image(x_fake,sample_dir+'/ep%d_%d.jpg'%(ep,it_g), nrow=8)
+	    if True:
+	        #x_fake = (sample(z)+1)/2
+	        torch.no_grad:
+	            x_fake = sample(z)
+	            torchvision.utils.save_image(x_fake,sample_dir+'/ep%d.jpg'%(ep), nrow=8)
 	    # save checkpoint
 	    if (ep+1)%5==0:
 	        torch.save(G.state_dict(), ckpt_dir+'/Epoch_G_(%d).pth' % ep)
