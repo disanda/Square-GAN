51c671304586375d91636112a83df07bd5ab69bc
disanda <skyanda1@qq.com>
2020-09-21 16:41:53 +0800

    x

--- loss_func.py
+++ loss_func.py
@@ -31,8 +31,8 @@ def get_hinge_v1_losses_fn():
 
 def get_hinge_v2_losses_fn():
     def d_loss_fn(r_logit, f_logit):
-        r_loss = torch.max(0.5 - r_logit, torch.zeros_like(r_logit)).mean()
-        f_loss = torch.max(0.5 + f_logit, torch.zeros_like(f_logit)).mean()
+        r_loss = torch.max(- r_logit, torch.zeros_like(r_logit)).mean()
+        f_loss = torch.max(f_logit, torch.zeros_like(f_logit)).mean()
         return r_loss, f_loss
 
     def g_loss_fn(f_logit):
