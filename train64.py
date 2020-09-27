05464d094ab7adbf0fc930b45807ec2160703b89
disanda <skyanda1@qq.com>
2020-09-28 00:09:24 +0800

    x

--- train64.py
+++ train64.py
@@ -87,8 +87,8 @@ D = networks.ConvDiscriminator(shape[-1], n_downsamplings=n_D_downsamplings, nor
 
 def get_hinge_v2_1():
     def d_loss_fn(r_logit, f_logit):
-        r_loss = torch.max(5- r_logit, torch.zeros_like(r_logit)).mean()
-        f_loss = torch.max(5+ f_logit, torch.zeros_like(f_logit)).mean()
+        r_loss = torch.max(0.5- r_logit, torch.zeros_like(r_logit)).mean()
+        f_loss = torch.max(0.5+ f_logit, torch.zeros_like(f_logit)).mean()
         return r_loss, f_loss
     def g_loss_fn(f_logit):
         f_loss = -f_logit.mean()
@@ -98,8 +98,8 @@ def get_hinge_v2_1():
 
 def get_hinge_v2_2():
     def d_loss_fn(r_logit, f_logit):
-        r_loss = torch.max(4.5- r_logit, torch.zeros_like(r_logit)).mean()
-        f_loss = torch.max(4.5+ f_logit, torch.zeros_like(f_logit)).mean()
+        r_loss = torch.max(0.44- r_logit, torch.zeros_like(r_logit)).mean()
+        f_loss = torch.max(0.44+ f_logit, torch.zeros_like(f_logit)).mean()
         return r_loss, f_loss
     def g_loss_fn(f_logit):
         f_loss = -f_logit.mean()
@@ -108,8 +108,8 @@ def get_hinge_v2_2():
 
 def get_hinge_v2_3():
     def d_loss_fn(r_logit, f_logit):
-        r_loss = torch.max(4- r_logit, torch.zeros_like(r_logit)).mean()
-        f_loss = torch.max(4+ f_logit, torch.zeros_like(f_logit)).mean()
+        r_loss = torch.max(0.42- r_logit, torch.zeros_like(r_logit)).mean()
+        f_loss = torch.max(0.42+ f_logit, torch.zeros_like(f_logit)).mean()
         return r_loss, f_loss
     def g_loss_fn(f_logit):
         f_loss = -f_logit.mean()
@@ -118,8 +118,8 @@ def get_hinge_v2_3():
 
 def get_hinge_v2_4():
     def d_loss_fn(r_logit, f_logit):
-        r_loss = torch.max(3.5- r_logit, torch.zeros_like(r_logit)).mean()
-        f_loss = torch.max(3.5+ f_logit, torch.zeros_like(f_logit)).mean()
+        r_loss = torch.max(0.405- r_logit, torch.zeros_like(r_logit)).mean()
+        f_loss = torch.max(0.405+ f_logit, torch.zeros_like(f_logit)).mean()
         return r_loss, f_loss
     def g_loss_fn(f_logit):
         f_loss = -f_logit.mean()
@@ -128,8 +128,8 @@ def get_hinge_v2_4():
 
 def get_hinge_v2_5():
     def d_loss_fn(r_logit, f_logit):
-        r_loss = torch.max(3- r_logit, torch.zeros_like(r_logit)).mean()
-        f_loss = torch.max(3+ f_logit, torch.zeros_like(f_logit)).mean()
+        r_loss = torch.max(0.40- r_logit, torch.zeros_like(r_logit)).mean()
+        f_loss = torch.max(0.40+ f_logit, torch.zeros_like(f_logit)).mean()
         return r_loss, f_loss
     def g_loss_fn(f_logit):
         f_loss = -f_logit.mean()
@@ -148,8 +148,15 @@ d_loss_fn_4,g_loss_fn_4 = get_hinge_v2_4()
 d_loss_fn_5,g_loss_fn_5 = get_hinge_v2_5()
 
 # optimizer
-G_optimizer = torch.optim.Adam(G.parameters(), lr=args.lr, betas=(args.beta_1, 0.999))
-D_optimizer = torch.optim.Adam(D.parameters(), lr=args.lr, betas=(args.beta_1, 0.999))
+#G_optimizer = torch.optim.Adam(G.parameters(), lr=args.lr, betas=(args.beta_1, 0.999))
+#D_optimizer = torch.optim.Adam(D.parameters(), lr=args.lr, betas=(args.beta_1, 0.999))
+
+G_optimizer = torch.optim.SGD(G.parameters(), lr=0.0001, momentum=0.9)
+D_optimizer = torch.optim.SGD(D.parameters(), lr=0.0001, momentum=0.9)
+#momentum (float, optional): momentum factor (default: 0)
+#weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+#dampening (float, optional): dampening for momentum (default: 0)
+#nesterov (bool, optional): enables Nesterov momentum (default: False)
 
 @torch.no_grad()
 def sample(z):
