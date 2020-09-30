3113b5719d7574d499665a276961f36fbf1456a5
disanda <skyanda1@qq.com>
2020-09-30 14:26:13 +0800

    x

--- train128.py
+++ train128.py
@@ -182,10 +182,11 @@ if __name__ == '__main__':
 
 #--------------save---------------
 	        #x_fake = (sample(z)+1)/2
-	    with torch.no_grad():
-	        z_t = torch.randn(64, args.z_dim, 1, 1).to(device)
-	        x_fake = sample(z_t)
-	        torchvision.utils.save_image(x_fake,sample_dir+'/ep%d.jpg'%(ep), nrow=8)
+	        if it_g%100==0:
+	            with torch.no_grad():
+	                z_t = torch.randn(64, args.z_dim, 1, 1).to(device)
+	                x_fake = sample(z_t)
+	                torchvision.utils.save_image(x_fake,sample_dir+'/ep_%d_%d.jpg'%(ep,it_g), nrow=8)
 	    # save checkpoint
 	    if (ep+1)%10==0:
 	        torch.save(G.state_dict(), ckpt_dir+'/Epoch_G_(%d).pth' % ep)
