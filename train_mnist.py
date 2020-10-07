298fb0346e757e824089812b3824af4232ba058e
disanda <skyanda1@qq.com>
2020-10-07 14:13:22 +0800

    x

--- train_mnist.py
+++ train_mnist.py
@@ -69,7 +69,7 @@ print('data-size:    '+str(shape))
 # ==============================================================================
 
 # networks
-G = net.Generator(input_channels = 1, feature_maps=args.img_size).to(device)
+G = net.Generator(output_channels = 1, feature_maps=args.img_size).to(device)
 D = net.Discriminator(input_channels = 1, feature_maps=args.img_size).to(device)
 #print(G)
 #print(D)
