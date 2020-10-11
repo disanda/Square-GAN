c3b3bd854c46befce5d1bd97aee12cb19e1ce448
disanda <skyanda1@qq.com>
2020-10-12 01:19:32 +0800

    x

--- data.py
+++ data.py
@@ -50,6 +50,7 @@ def make_dataset(dataset_name, batch_size,img_size,drop_remainder=True, shuffle=
         #dataset = DatasetFromFolder(path='/_yucheng/dataSet/pose/pose_set_10',size=img_size)
         #dataset = DatasetFromFolder(path='./data/Pose/pose_set_10',size=img_size)
         path_pose10='./data/Pose/pose_set_10'
+        dataset = DatasetFromFolder(path=path_pose10,transform=transform_pose10)
         img_shape = [img_size, img_size, 1]
     elif dataset_name == 'celeba_64':
         crop_size = 108
@@ -74,7 +75,7 @@ def make_dataset(dataset_name, batch_size,img_size,drop_remainder=True, shuffle=
         path_128 = 'F:/dataSet2/CelebAMask-HQ/CelebA-HQ-img'
         #path_128 = '/home/disanda/Desktop/dataSet/celeba-hq-download/celeba-128'
         #path_128 = '/_yucheng/dataSet/CelebAMask-HQ/CelebAMask-HQ/CelebA-HQ-img'
-        dataset = DatasetFromFolder(path=path_pose10,transform=transform_pose10)
+        dataset = DatasetFromFolder(path=path_128,transform=transform_128)
         img_shape = (img_size, img_size, 3)
     else:
         raise NotImplementedError
