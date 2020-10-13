dc4e714d4d565b27d647a07892902246c7309902
disanda <skyanda1@qq.com>
2020-10-13 17:34:30 +0800

    x

--- train_pose.py
+++ train_pose.py
@@ -218,8 +218,8 @@ if __name__ == '__main__':
 	        #r_loss = torch.max( 0.1+(args.epochs-ep)//args.epochs - x_real_d_logit, torch.zeros_like(x_real_d_logit)).mean()#((args.epochs-ep)//args.epochs) # Round_gp
 	        #f_loss = torch.max( 0.1+(args.epochs-ep)//args.epochs + x_fake_d_logit, torch.zeros_like(x_fake_d_logit)).mean()
 	        
-	        r_loss = torch.max( torch.randn(1) - x_real_d_logit.to(cpu), torch.zeros_like(x_real_d_logit)).mean() #shift_randomD 
-	        f_loss = torch.max( torch.randn(1) + x_real_d_logit.to(cpu), torch.zeros_like(x_real_d_logit)).mean()
+	        r_loss = torch.max( torch.randn(1) - x_real_d_logit.to('cpu'), torch.zeros_like(x_real_d_logit)).mean() #shift_randomD 
+	        f_loss = torch.max( torch.randn(1) + x_real_d_logit.to('cpu'), torch.zeros_like(x_real_d_logit)).mean()
 
 	        gp = g_penal.gradient_penalty(functools.partial(D), x_real, x_fake.detach(), gp_mode=args.gradient_penalty_mode, sample_mode=args.gradient_penalty_sample_mode)
 	        D_loss = (r_loss + f_loss) + gp * args.gradient_penalty_weight
