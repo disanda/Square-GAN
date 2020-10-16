bda51d7a6859758be6dcb1f74c9765772e2a2561
disanda <skyanda1@qq.com>
2020-10-17 03:48:29 +0800

    x

--- train_pose_LJFZ.py
+++ train_pose_LJFZ.py
@@ -231,7 +231,7 @@ if __name__ == '__main__':
 	            r_loss = torch.max(1 - (args.epochs-ep)/args.epochs*x_real_d_logit, torch.zeros_like(x_real_d_logit)).mean()
 	            f_loss = torch.max(1 + (args.epochs-ep)/args.epochs*x_fake_d_logit, torch.zeros_like(x_fake_d_logit)).mean()
 	        else:
-                r_loss = torch.max(0.5 + (args.epochs-ep)/args.epochs*x_real_d_logit, torch.zeros_like(x_real_d_logit)).mean()
+	            r_loss = torch.max(0.5 + (args.epochs-ep)/args.epochs*x_real_d_logit, torch.zeros_like(x_real_d_logit)).mean()
 	            f_loss = torch.max(0.5 - (args.epochs-ep)/args.epochs*x_fake_d_logit, torch.zeros_like(x_fake_d_logit)).mean()
 
 	        gp = g_penal.gradient_penalty(functools.partial(D), x_real, x_fake.detach(), gp_mode=args.gradient_penalty_mode, sample_mode=args.gradient_penalty_sample_mode)
