d417d4fc95bfe2dfed7da3f72830b5c7f351c744
disanda <skyanda1@qq.com>
2020-10-16 21:45:30 +0800

    x

--- networks/network_origin.py
+++ networks/network_origin.py
@@ -33,6 +33,7 @@ class ConvGenerator(nn.Module):
                 ):
         super().__init__()
         Norm = _get_norm_layer_2d(norm)
+        self.bias = biaS
         def dconv_norm_relu(in_dim, out_dim, kernel_size=4, stride=2, padding=1,biass=False):
             return nn.Sequential(
                 nn.ConvTranspose2d(in_dim, out_dim, kernel_size, stride=stride, padding=padding, bias=biass),
@@ -42,13 +43,13 @@ class ConvGenerator(nn.Module):
         layers = []
         # 1: 1x1 -> 4x4
         d = min(dim * 2 ** (n_upsamplings - 1), dim * 8)
-        layers.append(dconv_norm_relu(input_dim, d, kernel_size=4, stride=1, padding=0, biass = biaS))
+        layers.append(dconv_norm_relu(input_dim, d, kernel_size=4, stride=1, padding=0, biass = self.bias))
         # 2: upsamplings, 4x4 -> 8x8 -> 16x16 -> ...
         for i in range(n_upsamplings - 1):
             d_last = d
             d = d//2
-            layers.append(dconv_norm_relu(d_last, d, kernel_size=4, stride=2, padding=1,biass=biaS))
-        layers.append(nn.ConvTranspose2d(d, output_channels, kernel_size=4, stride=2, padding=1, bias=biaS))
+            layers.append(dconv_norm_relu(d_last, d, kernel_size=4, stride=2, padding=1,biass=self.bias))
+        layers.append(nn.ConvTranspose2d(d, output_channels, kernel_size=4, stride=2, padding=1, bias=self.bias))
         layers.append(nn.Tanh())
         self.net = nn.Sequential(*layers)
     def forward(self, z):
@@ -65,6 +66,7 @@ class ConvDiscriminator(nn.Module):
                  ):
         super().__init__()
         Norm = _get_norm_layer_2d(norm)
+        self.bias = biaS
         def conv_norm_lrelu(in_dim, out_dim, kernel_size=4, stride=2, padding=1, biass=False):
             return nn.Sequential(
                 nn.Conv2d(in_dim, out_dim, kernel_size, stride=stride, padding=padding, bias=biass),
@@ -74,14 +76,14 @@ class ConvDiscriminator(nn.Module):
         layers = []
         # 1: downsamplings, ... -> 16x16 -> 8x8 -> 4x4
         d = dim
-        layers.append(nn.Conv2d(input_channels, d, kernel_size=4, stride=2, padding=1, bias=biaS))
+        layers.append(nn.Conv2d(input_channels, d, kernel_size=4, stride=2, padding=1, bias=self.bias))
         layers.append(nn.LeakyReLU(0.2))
         for i in range(n_downsamplings - 1):
             d_last = d
             d = d*2 # 1 > 2 > 4 > 8  > 16 (64*64) > 32 > 64 >128 > 256 (1024*1024) 
-            layers.append(conv_norm_lrelu(d_last, d, kernel_size=4, stride=2, padding=1, biass=biaS))
+            layers.append(conv_norm_lrelu(d_last, d, kernel_size=4, stride=2, padding=1, biass=self.bias))
         # 2: logit
-        layers.append(nn.Conv2d(d, 1, kernel_size=4, stride=1, padding=0, bias=biaS))
+        layers.append(nn.Conv2d(d, 1, kernel_size=4, stride=1, padding=0, bias=self.bias))
         self.net = nn.Sequential(*layers)
     def forward(self, x):
         y = self.net(x)
