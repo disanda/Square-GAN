920545f95c10ea4d0c06e311757d2d4350cda06a
disanda <skyanda1@qq.com>
2020-11-22 22:10:52 +0800

    x

--- networks/D2E_v2.py
+++ networks/D2E_v2.py
@@ -55,7 +55,7 @@ class Generator(nn.Module):
         layers.append(nn.ConvTranspose2d(512,256,kernel_size=1, bias=bias_flag))
         layers.append(nn.Tanh())
 
-        layers.append(nn.ConvTranspose2d(256,3, kernel_size=4,stride=2, bias=bias_flag))
+        layers.append(nn.ConvTranspose2d(256,3, kernel_size=4,stride=2, padding=1, bias=bias_flag))
         layers.append(nn.Tanh())
 
         # all
@@ -74,7 +74,7 @@ class Discriminator_SpectrualNorm(nn.Module):
         bias_flag = False
 
         # 1:
-        layers.append(spectral_norm(nn.Conv2d(3, 256, kernel_size=4, stride=2, bias=bias_flag)))
+        layers.append(spectral_norm(nn.Conv2d(3, 256, kernel_size=4, stride=2, padding=1, bias=bias_flag)))
         layers.append(nn.LeakyReLU(0.2, inplace=True))
 
         layers.append(spectral_norm(nn.Conv2d(256, 512, kernel_size=1, bias=bias_flag)))
